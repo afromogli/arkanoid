da27b7588bfff7dee566843e74a31a087636a076
Aram Taieb <aram.paul.taieb@gmail.com>
2017-10-09 16:40:07 +0200

    - Fixed more consistent naming of private members

--- Arkanoid/Arkanoid/Rect2d.cpp
+++ Arkanoid/Arkanoid/Rect2d.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 namespace Arkanoid {
    bool Rect2D::intersects(const Rect2D& otherBBox) const {
-      float xDiff = fabs((position.x + width / 2)- (otherBBox.position.x + otherBBox.getWidth() / 2));
-      float yDiff = fabs((position.y + height / 2) - (otherBBox.position.y+ otherBBox.getHeight() / 2));
+      float xDiff = fabs((m_position.x + m_width / 2)- (otherBBox.m_position.x + otherBBox.getWidth() / 2));
+      float yDiff = fabs((m_position.y + m_height / 2) - (otherBBox.m_position.y+ otherBBox.getHeight() / 2));
 
-      bool xIntersects = xDiff < (width/2 + otherBBox.getWidth()/2);
-      bool yIntersects = yDiff < (height/2 + otherBBox.getHeight()/2);
+      bool xIntersects = xDiff < (m_width/2 + otherBBox.getWidth()/2);
+      bool yIntersects = yDiff < (m_height/2 + otherBBox.getHeight()/2);
 
 
      /* cout << "xDiff: " << xDiff << "\n";
--- Arkanoid/Arkanoid/Rect2d.h
+++ Arkanoid/Arkanoid/Rect2d.h
@@ -4,47 +4,47 @@
 
 namespace Arkanoid {
    class Rect2D {
-   private:
-      int width;
-      int height;
-      // The position is treated as the upper left corner of the bounding box
-      Vector2f position;
-
    public:
       Rect2D() {
-         width = 0;
-         height = 0;
-         position = Vector2f();
+         m_width = 0;
+         m_height = 0;
+         m_position = Vector2f();
       }
 
       Rect2D(const Vector2f& position, const int& width, const int& height) {
-         this->width = width; 
-         this->height = height;
-         this->position = position;
+         this->m_width = width; 
+         this->m_height = height;
+         this->m_position = position;
       }
 
       bool intersects(const Rect2D& otherBBox) const;
 
       const Vector2f& getPosition() const {
-         return position;
+         return m_position;
       }
 
       void setPosition(const Vector2f& position) {
-         this->position = position;
+         this->m_position = position;
       }
 
       const int getWidth() const {
-         return width;
+         return m_width;
       }
 
       const int getHeight() const {
-         return height;
+         return m_height;
       }
 
       void setSize(const int width, const int height)
       {
-         this->width = width;
-         this->height = height;
+         this->m_width = width;
+         this->m_height = height;
       }
+
+   private:
+      int m_width;
+      int m_height;
+      // The position is treated as the upper left corner of the bounding box
+      Vector2f m_position;
    };
 }
